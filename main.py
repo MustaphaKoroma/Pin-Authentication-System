4d9a01dfc2bdd920dc0bbe5d91338ba60319c0ed
Mustapha Koroma <mustaphakoroma@yahoo.com>
2026-01-28 13:51:00 +0000

    Update main.py
    Add application entry point in main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Application entry point
+from auth import hash_pin, authenticate_user
+
+def main():
+    # Example PIN: 1234
+    stored_pin_hash = hash_pin("1234")
+    authenticate_user(stored_pin_hash)
+
+if __name__ == "__main__":
+    main()
